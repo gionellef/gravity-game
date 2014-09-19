6a8e0719e72c3793271b458c5039b3418f3b1284
GioPogi <gionellef@gmail.com>
2014-09-19 16:23:12 +0800

    Add class GameObject

---
+++ src/com/megahard/gravity/GameObject.java
@@ -0,0 +1,13 @@
+package com.megahard.gravity;
+
+import java.awt.Graphics;
+import java.util.Vector;
+
+public class GameObject {
+	public boolean floats;
+	public Vector<Double> position;
+	public Vector<Double> velocity;
+	public Vector<Double> size;
+	public float mass;
+	public Graphics graphics;
+}
