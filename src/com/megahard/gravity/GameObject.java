031005c4bc19e467cea3f3e58042e33d97fc5aa1
GioPogi <gionellef@gmail.com>
2014-09-19 16:53:06 +0800

    Change Vector to Vector2

--- src/com/megahard/gravity/GameObject.java
+++ src/com/megahard/gravity/GameObject.java
@@ -1,13 +1,12 @@
 package com.megahard.gravity;
 
 import java.awt.Graphics;
-import java.util.Vector;
 
 public class GameObject {
 	public boolean floats;
-	public Vector<Double> position;
-	public Vector<Double> velocity;
-	public Vector<Double> size;
+	public Vector2 position;
+	public Vector2 velocity;
+	public Vector2 size;
 	public float mass;
 	public Graphics graphics;
 }
