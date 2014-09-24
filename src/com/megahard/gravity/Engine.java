24a25b135d0dfadd2b384ecb7f4d2d564ba90e83
Kalabasa <godffrey0@gmail.com>
2014-09-24 14:09:37 +0800

    Edit Engine para per-object yung update.

--- src/com/megahard/gravity/Engine.java
+++ src/com/megahard/gravity/Engine.java
@@ -1,44 +1,19 @@
 package com.megahard.gravity;
 
-public class Engine {
+import java.util.LinkedList;
+import java.util.List;
 
-	public void moveLeft(GameObject obj){
-		//check if object doesn't exceed left limit of map
-		if(obj.position.x - obj.velocity.x > 0){
-			obj.position.x = obj.position.x - obj.velocity.x;
-		}
-		else{
-			obj.position.x = 0;
-		}
+public class Engine {
+	public GameMap map;
+	public List<GameObject> objects;
+
+	public Engine(String levelData) {
+		objects = new LinkedList<>();
 	}
-	
-	public void moveRight(GameState gamestate, GameObject obj){
-		//check if object doesn't exceed right limit of map
-		if(obj.position.x + obj.velocity.x < gamestate.map.getWidth()){
-			obj.position.x = obj.position.x + obj.velocity.x;
-		}
-		else{
-			obj.position.x = gamestate.map.getWidth() - 1;
-		}
-	}
-	
-	public void moveUp(GameState gamestate, GameObject obj){
-		//check if object doesn't exceed upper limit of map
-		if(obj.position.y + obj.velocity.y < gamestate.map.getHeight()){
-			obj.position.y = obj.position.y + obj.velocity.y;
-		}
-		else{
-			obj.position.y = gamestate.map.getHeight() - 1;
-		}
-	}
-	
-	public void moveDown(GameObject obj){
-		//check if object doesn't exceed lower limit of map
-		if(obj.position.y - obj.velocity.y > 0){
-			obj.position.y = obj.position.y - obj.velocity.y;
-		}
-		else{
-			obj.position.y = 0;
+
+	public void update() {
+		for (GameObject o : objects) {
+			o.update();
 		}
 	}
 }
