9d200e4590c3e9426f1198696c7d50d9184aa231
Kalabasa <godffrey0@gmail.com>
2014-09-24 14:06:37 +0800

    Add Tile enum to game map

--- src/com/megahard/gravity/GameMap.java
+++ src/com/megahard/gravity/GameMap.java
@@ -2,17 +2,31 @@ package com.megahard.gravity;
 
 public class GameMap {
 
+	public enum Tile {
+		Air(false, "air.png"),
+		Floor(true, "floor.png");
+
+		private final boolean collidable;
+		private final String graphics;
+
+		Tile(boolean collidable, String graphics) {
+			this.collidable = collidable;
+			this.graphics = graphics;
+		}
+
+	}
+
 	public static final int TILE_SIZE = 32;
 
 	private int width;
 	private int height;
-	private int[] map;
+	private Tile[] map;
 
-	public GameMap(int width, int height, int[] level) {
+	public GameMap(int width, int height, Tile[] level) {
 		this.width = width;
 		this.height = height;
 		// tiles are arranged in 2D array in row major order
-		map = new int[width * height];
+		map = new Tile[width * height];
 		for (int i = 0; i < level.length; i++) {
 			map[i] = level[i];
 		}
@@ -26,11 +40,11 @@ public class GameMap {
 		return height;
 	}
 
-	public int getTile(int x, int y) {
+	public Tile getTile(int x, int y) {
 		return map[y * width + x];
 	}
 
-	public void setTile(int x, int y, int value) {
+	public void setTile(int x, int y, Tile value) {
 		map[y + width + x] = value;
 	}
 }
