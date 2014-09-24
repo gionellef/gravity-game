7c12a1f205c3ea15ae432543fe089594f571e29e
Kalabasa <godffrey0@gmail.com>
2014-09-24 14:38:05 +0800

    Add update method

--- src/com/megahard/gravity/GameObject.java
+++ src/com/megahard/gravity/GameObject.java
@@ -3,10 +3,19 @@ package com.megahard.gravity;
 import java.awt.Graphics;
 
 public class GameObject {
-	public boolean floats;
 	public Vector2 position;
 	public Vector2 velocity;
 	public Vector2 size;
 	public double mass;
 	public Graphics graphics;
+
+	public GameObject() {
+	}
+
+	public void update() {
+		position = position.translate(velocity); // movement due to velocity
+		velocity.y += 1; // acceleration due to gravity
+
+		// COLLISION DETECTION GOES HERE
+	}
 }
