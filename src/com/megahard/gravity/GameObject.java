4b0cb8445c54bbb78993a2fdbbfce6548980a845
Kalabasa <godffrey0@gmail.com>
2014-09-24 14:57:09 +0800

    Give Object a reference to the Engine for things

--- src/com/megahard/gravity/GameObject.java
+++ src/com/megahard/gravity/GameObject.java
@@ -3,19 +3,25 @@ package com.megahard.gravity;
 import java.awt.Graphics;
 
 public class GameObject {
+	private final Engine game;
+
 	public Vector2 position;
 	public Vector2 velocity;
 	public Vector2 size;
 	public double mass;
 	public Graphics graphics;
 
-	public GameObject() {
+	public GameObject(Engine game) {
+		this.game = game;
 	}
 
 	public void update() {
 		position = position.translate(velocity); // movement due to velocity
 		velocity.y += 1; // acceleration due to gravity
 
-		// COLLISION DETECTION GOES HERE
+		// COLLISION DETECTION WITH MAP GOES HERE
+		// collision detection algorithm
+		// get tiles at corners
+		// if solid, push out
 	}
 }
