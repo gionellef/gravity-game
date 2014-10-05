5c646c00c3acca5eeb06c4a87500ce578be79da5
GioPogi <gionellef@gmail.com>
2014-10-06 00:15:31 +0800

    Fix DoubleBuffering [flixkrIssue]

--- src/com/megahard/gravity/GravityApplet.java
+++ src/com/megahard/gravity/GravityApplet.java
@@ -1,110 +1,28 @@
 package com.megahard.gravity;
 
 import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 
+import javax.swing.JFrame;
+
 public class GravityApplet extends Applet {
 	
-	private int bufferWidth;
-	private int bufferHeight;
-	private Image bufferImage;
-	private Graphics bufferGraphics;
+	private Renderer renderer = new Renderer();
 	
-	public Engine engine;
-	public Renderer renderer;
-
 	@Override
 	public void init() {
-		engine = new Engine();
-		renderer = new Renderer();
-		engine.initialize("");
-
-		// The render loop thread
-		new Thread(){
-			@Override
-			public void run() {
-
-				boolean ever = true;
-				for(;ever;){
-					engine.update();
-
-					repaint();
-					try {
-						Thread.sleep(20);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-		}.start();
-
+		setLayout(new BorderLayout());
+		add(renderer, BorderLayout.CENTER);
 	}
 	
-	/* Idea of
-	 * DOUBLEBUFFERING YO
-	 * First, paint everything into an off-screen panel then when it's ready,
-	 * paint the completed off-screen image into the current on-screen panel.
-	 */
-	@Override
-	public void paint(Graphics g) {
-		
-		// checks buffersize of offscreen panel with onscreen panel
-		// important when onscreen panel is resized, so offscreen will follow
-		if (bufferWidth!=getSize().width || 
-			bufferHeight!=getSize().height ||
-			bufferImage==null || bufferGraphics==null) {
-			
-			// makes off screen and on screen dimensions same
-			resetBuffer();
-		}
-		
-		// clears off screen panel if something is drawn
-		if (bufferGraphics != null) {
-			bufferGraphics.clearRect(0, 0, bufferWidth, bufferHeight);
-			
-			// paint into offscren panel
-			paintBuffer(bufferGraphics);
-			
-			// paint into onscreen panel
-			g.drawImage(bufferImage, 0, 0, this);
-		}
+	public void start() {
+		renderer.start();
 	}
 	
-	private void resetBuffer() {
-		// to keep track of image size
-		bufferWidth = getSize().width;
-		bufferHeight = getSize().height;
-		
-		// clean up previous image
-		if (bufferGraphics != null) {
-			bufferGraphics.dispose();
-			bufferGraphics = null;
-		}
-		if (bufferImage != null) {
-			bufferImage.flush();
-			bufferImage = null;
-		}
-		
-		// Garbage collector, yea!
-		System.gc(); //noononononononono. manual gc makes things slow.
-		
-		// create the new image in the size of the panel
-		bufferImage = createImage (bufferWidth,bufferHeight);
-		bufferGraphics = bufferImage.getGraphics();
-	}
-	
-	public void paintBuffer(Graphics g) {
-		renderer.render(g, engine.getState());
-	}
-	
-	/*
-	 * Override the update method when calling repaint() which clears the panel
-	 * Routing it straight to paint without clearing panel so no flicker, yea?
-	 */
-	@Override
-	public void update(Graphics g) {
-		paint(g);
+	public void stop() {
+		renderer.stop();
 	}
 }
--- src/com/megahard/gravity/Renderer.java
+++ src/com/megahard/gravity/Renderer.java
@@ -1,16 +1,56 @@
 package com.megahard.gravity;
 
+import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
 
 import com.megahard.gravity.GameMap.Tile;
 
-public class Renderer {
-	public void render(Graphics g, GameState s) {
-		GameMap map = s.map;
+public class Renderer extends Canvas implements Runnable {
+	private int bufferWidth;
+	private int bufferHeight;
+	private Image bufferImage;
+	
+	private Engine engine;
+	public GameMap map;
+	
+	private boolean running = false;
+	
+	private static final int TILE_SIZE = 16;
+	
+	
+	public void start() {
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop() {
+		running = false;
+	}
+	
+	public void render(GameState s) {
 
-		int TILE_SIZE = 16;
-
+		BufferStrategy bs = getBufferStrategy();
+		
+		if (bs == null) {
+			createBufferStrategy (2);
+			requestFocus();
+			return;
+		}
+		
+		if (bs.getDrawGraphics() == null)
+			running = false;
+		Graphics g = bs.getDrawGraphics();
+			
+			
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
 		for (int y = 0; y < map.getHeight(); y++) {
 			for (int x = 0; x < map.getWidth(); x++) {
 				Tile tile = map.getTile(x, y);
@@ -24,5 +64,33 @@ public class Renderer {
 			g.fillRect((int)((o.position.x - o.size.x/2) * TILE_SIZE), (int)((o.position.y - o.size.y/2) * TILE_SIZE), 
 					(int)(o.size.x * TILE_SIZE), (int)(o.size.y * TILE_SIZE));
 		}
+		
+		g.dispose();
+		bs.show();
 	}
+
+	private void init() {
+		engine = new Engine();
+		engine.initialize("");
+		GameState s = engine.getState();
+		map = s.map;
+	}
+	
+	@Override
+	public void run() {
+		init();
+		while(running){
+			engine.update();
+			render(engine.getState());
+
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
 }
