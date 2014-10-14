012a7b46d6b8bb2218618f00e9a4d69798c05334
Kalabasa <godffrey0@gmail.com>
2014-10-14 14:25:25 +0800

    Remove comments na mapanget

--- src/com/megahard/gravity/Engine.java
+++ src/com/megahard/gravity/Engine.java
@@ -88,21 +88,13 @@ public class Engine{
 		for (GameObject o : state.objects) {
 			for (int i = state.objects.indexOf(o) + 1; i < state.objects.size(); i++) {
 				GameObject o2 = state.objects.get(i);
-//				System.out.println("x " + o.position.x + " y " + o2.position.x);
-				//				System.out.println((o.position.x < o2.position.x + o2.size.x) + " "
-				//						+ (o.position.x  + o.size.x < o2.position.x) + " "
-				//						+ (o.position.y < o2.position.y + o2.size.y) + " "
-				//						+ (o.position.y  + o.size.y < o2.position.y));
 
 				if (o.position.x - o.size.x/2 < o2.position.x + o2.size.x/2
 					&& o.position.x  + o.size.x/2 > o2.position.x - o2.size.x/2
 					&& o.position.y - o.size.y/2 < o2.position.y + o2.size.y/2
 					&& o.position.y + o.size.y/2 > o2.position.y - o2.size.y/2) {
-					// collision detected
-					//					System.out.println("!!!!");
 					o.collide(o2);
-				} else {
-					//					System.out.println("a");
+					// o2.collide(o) ???
 				}
 			}
 		}
