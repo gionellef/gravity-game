c7964c880f86a7b8207631e41217992d5314f062
Kalabasa <godffrey0@gmail.com>
2014-10-30 18:57:29 +0800

    Add Player and GravWell class Add "fixed" property to GameObject for static objects

---
+++ src/com/megahard/gravity/objects/Player.java
@@ -0,0 +1,12 @@
+package com.megahard.gravity.objects;
+
+import com.megahard.gravity.Engine;
+import com.megahard.gravity.GameObject;
+
+public class Player extends GameObject {
+
+	public Player(Engine game) {
+		super(game, "");
+	}
+
+}
