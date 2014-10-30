84fa4b87057252f45ebf91478bf67f20f01188d8
Kalabasa <godffrey0@gmail.com>
2014-10-30 15:31:13 +0800

    Remove key handling in Renderer class

--- src/com/megahard/gravity/Renderer.java
+++ src/com/megahard/gravity/Renderer.java
@@ -3,52 +3,20 @@ package com.megahard.gravity;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
 import com.megahard.gravity.GameMap.Tile;
 
-public class Renderer extends Canvas implements KeyListener {
+public class Renderer extends Canvas {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private Vector2 camera;
-
-	public enum Action {
-		UP, DOWN, LEFT, RIGHT, JUMP, NONE;
-
-		public String value() {
-			if (this == UP) {
-				return "up";
-			}
-			if (this == DOWN) {
-				return "down";
-			}
-			if (this == LEFT) {
-				return "left";
-			}
-			if (this == RIGHT) {
-				return "right";
-			}
-			if (this == JUMP) {
-				return "jump";
-			}
-			return "";
-		}
-	}
-
-	public Action action;
 
 	private static final int TILE_SIZE = 16;
 
 	public Renderer() {
 		camera = new Vector2();
-		action = Action.NONE;
-		addKeyListener(this);
 	}
 
 	public void render(GameState s) {
@@ -104,45 +72,6 @@ public class Renderer extends Canvas implements KeyListener {
 
 		g.dispose();
 		bs.show();
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_W:
-//			System.out.println("up");
-			action = Action.UP;
-			break;
-		case KeyEvent.VK_A:
-//			System.out.println("left");
-			action = Action.LEFT;
-			break;
-		case KeyEvent.VK_S:
-//			System.out.println("down");
-			action = Action.DOWN;
-			break;
-		case KeyEvent.VK_D:
-//			System.out.println("right");
-			action = Action.RIGHT;
-			break;
-		case KeyEvent.VK_SPACE:
-//			System.out.println("jump");
-			action = Action.RIGHT;
-			break;
-		default:
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TEST CODE
-		action = Action.NONE;
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	public Vector2 getCamera() {
