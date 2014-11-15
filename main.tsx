cf0518843685b15c89b07695400611a96556ee82
Kalabasa <godffrey0@gmail.com>
2014-11-15 19:56:05 +0800

    Add main tileset

---
+++ main.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="main" tilewidth="16" tileheight="16">
+ <image source="res/tileset.png" width="640" height="320"/>
+ <terraintypes>
+  <terrain name="solid" tile="41"/>
+  <terrain name="background" tile="443"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="40" terrain=",0,,0"/>
+ <tile id="41" terrain="0,0,0,0"/>
+ <tile id="42" terrain="0,,0,"/>
+ <tile id="80" terrain=",0,,"/>
+ <tile id="81" terrain="0,0,,"/>
+ <tile id="82" terrain="0,,,"/>
+ <tile id="120" terrain="0,0,0,"/>
+ <tile id="121" terrain="0,0,,0"/>
+ <tile id="160" terrain="0,,0,0"/>
+ <tile id="161" terrain=",0,0,0"/>
+ <tile id="443" terrain="1,1,1,1"/>
+</tileset>
