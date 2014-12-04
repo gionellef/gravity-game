8a9b95a5d7cfc95991f65d7224948743dc4bea82
Kalabasa <godffrey0@gmail.com>
2014-12-05 03:19:48 +0800

    add planet

--- src/com/megahard/gravity/scripts/Finale.java
+++ src/com/megahard/gravity/scripts/Finale.java
@@ -23,27 +23,36 @@ public class Finale extends ScriptSequencer {
 
 	@Override
 	public void onStart() {
-		// talk talk talk
 		addMessage("isaac", "This must be the control center.", 100);
 
 		// send message
 		addMessage("isaac", "I need to send this message to Gravitonia fast.", 80);
-		addMessage("isaac", "...", 50);
+
+		// center camera
+		addRunnable(new Runnable() {
+			@Override
+			public void run() {
+				getGame().setCameraTarget(getCenter().plus(0, 7));
+			}
+		}, 0);
+		
+		addMessage("isaac", "...", 70);
 		addMessage("isaac", "Done, I hope they got my message.", 90);
 
 		// fade to black
 		addRunnable(new Runnable() {
 			@Override
 			public void run() {
-				getGame().setCameraTarget(getCenter().plus(0, 7));
-				getGame().fadeScreen(null, Color.black, 100);
+				getGame().fadeScreen(null, Color.black, 50);
 			}
-		}, 100);
-		
-		addMessage("PlanetGravitonia", "", 60);
+		}, 50);
+
+		addDelay(10);
+
+		addMessage("PlanetGravitonia", "On Gravitonia...", 90);
 		
 		// soldier
-		addMessage("gravitonian-soldier", "General Maxwell, a message came from x300, y125, h123, t2467 coordinates. It seems to be an Ytivargs' base. The message has Galileo's signiture in it.", 200);
+		addMessage("gravitonian-soldier", "General Maxwell, a message came from x300, y125, h123, t2467 coordinates. It seems to be an Ytivargs' base. The message has Galileo's signiture in it.", 250);
 		// Commander Maxwell
 		addMessage("maxwell", "Tell me the its contents quickly!", 100);
 		// soldier
@@ -53,12 +62,14 @@ public class Finale extends ScriptSequencer {
 		// Commander Maxwell
 		addMessage("maxwell", "Prepare the war ships and the Pendulum War Fortress! We are gonna F*** some Ytivargs' nig*as asses!!!", 180);
 
-		//addMessage("Ytivargland", "", 100);
+		addDelay(20);
+		
+		addMessage("PlanetYtivarg", "Meanwhile, on planet Ytivarngis, home of the Ytivargs...", 120);
 		
 		// Ytivarg soldier
-		addMessage("ytivarg", "Commander Swaggernaut, some dipsh*t sent an unauthorized message from the Arkthuri System. In the Zumtalan Gamma cluster, Mk'tharon outpost 316.", 190);
+		addMessage("ytivarg", "Commander Swaggernaut, some dipsh*t sent an unauthorized message from Mk'tharon outpost #316 in the Arkthuri System.", 200);
 		// Commander Swaggernaut
-		addMessage("swaggernaut", "Aight, what does it say mah homie dawg nig*a?", 100);
+		addMessage("swaggernaut", "Aight, what does it say mah homie dawg nig*a?", 120);
 		// Ytivarg soldier
 		addMessage("ytivarg", "The Earthling punk we ubducted sent a message to Gravitonia. He's asking for some help.", 120);
 		// Commander Swaggernaut
@@ -69,7 +80,9 @@ public class Finale extends ScriptSequencer {
 		addMessage("swaggernaut", "Stop chillin' mah brotahs. Those Gravitonian bastards got the message by now. Prepare mah nig*as. We are sending the Swag War Fleet. They ain't got no match for us!", 220);
 		// Ytivarg soldier
 		addMessage("ytivarg", "They gonna get #rekt!", 100);
-		
+
+		addDelay(50);
+
 		// Ending text
 		addMessage(null, "The real war is just about to begin", 200);
 		
